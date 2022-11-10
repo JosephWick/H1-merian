bfac1318018b6e3743f2e25e5c2b8e3ef9f2b28c
Joseph Wick <josephwick@outlook.com>
2022-11-09 17:27:30 -0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+import pynbody
+import pylab
+
+
+dpath = '/data/REPOSITORY/romulus_dwarf_zooms/r597.romulus25.3072g1HsbBH/'
+f = 'r597.romulus25.3072g1HsbBH.004096'
+
+s = pynbody.load(dpath+f)
+print('loaded')
