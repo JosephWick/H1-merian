bb28b1d9f4ed774d4c1b16d14438a8d6303aaeb2
Joseph Wick <josephwick@outlook.com>
2022-11-10 13:35:13 -0800

    Update test.py

--- test.py
+++ test.py
@@ -8,13 +8,12 @@ dpath = '/data/REPOSITORY/romulus_dwarf_zooms/r597.romulus25.3072g1HsbBH/'
 f = 'r597.romulus25.3072g1HsbBH.004096'
 
 s = pynbody.load(dpath+f)
-print('loaded')
 
 # get first halo
 h = s.halos()
 h1 = h[1]
 
-print('ngas = %e, ndark = %e, nstar = %e\n'%(len(h1.gas),len(h1.dark),len(h1.star)))
+#print('ngas = %e, ndark = %e, nstar = %e\n'%(len(h1.gas),len(h1.dark),len(h1.star)))
 
 # centering based on 1st halo
 cen_pot = pynbody.analysis.halo.center(h1, mode='pot', retcen=True)
@@ -22,4 +21,4 @@ s['pos'] -= cen_pot
 
 # a first image
 s.physical_units()
-pynbody.plot.image(h1.g, width=100, cmap='Blues', filename='figures/f1.png')
+pynbody.plot.image(h1.g, width=50, cmap='Blues', filename='figures/f1.png')
