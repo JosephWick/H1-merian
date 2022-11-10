8773d761535ddd20ffd7ebf06bb2886218df67fd
Joseph Wick <josephwick@outlook.com>
2022-11-09 17:57:03 -0800

    Update test.py

--- test.py
+++ test.py
@@ -4,5 +4,5 @@ import sys
 dpath = '/data/REPOSITORY/romulus_dwarf_zooms/r597.romulus25.3072g1HsbBH/'
 f = 'r597.romulus25.3072g1HsbBH.004096'
 
-s = pynbody.load(dpath+f)
+#s = pynbody.load(dpath+f)
 print('loaded')
