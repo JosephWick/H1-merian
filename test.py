77ede5f2bb01c46ec8f8ac7377e60e730ee950d6
Joseph Wick <josephwick@outlook.com>
2022-11-09 18:00:24 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import sys
 
 sys.path.insert(0, '/home/software/pynbody/pynbody/')
+import pynbody
+
 
 dpath = '/data/REPOSITORY/romulus_dwarf_zooms/r597.romulus25.3072g1HsbBH/'
 f = 'r597.romulus25.3072g1HsbBH.004096'
