4680698b86846ede8d1c4b351adda21c6fb48249
Joseph Wick <josephwick@outlook.com>
2022-11-10 13:20:21 -0800

    Update test.py

--- test.py
+++ test.py
@@ -10,7 +10,16 @@ f = 'r597.romulus25.3072g1HsbBH.004096'
 s = pynbody.load(dpath+f)
 print('loaded')
 
+# get first halo
 h = s.halos()
 h1 = h[1]
 
 print('ngas = %e, ndark = %e, nstar = %e\n'%(len(h1.gas),len(h1.dark),len(h1.star)))
+
+# centering based on 1st halo
+cen_pot = pynbody.analysis.halo.center(h1, mode='pot', retcen=True)
+s['pos'] -= cen_pot
+
+# a first image
+s.physical_units()
+pynbody.plot.image(h1.g, width=100, cmap='Blues', filename='figures/f1.png')
