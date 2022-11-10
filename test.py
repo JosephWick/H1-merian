121fb0a00f503a1d24c60d64e60dcd2973641cfa
Joseph Wick <josephwick@outlook.com>
2022-11-10 10:41:13 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 import sys
-sys.path.insert(0, '/home/software/pynbody/')
-import pynbody
-import pylab
+#sys.path.insert(0, '/home/software/pynbody/')
+#import pynbody
+#import pylab
+import numpy as np
 
 dpath = '/data/REPOSITORY/romulus_dwarf_zooms/r597.romulus25.3072g1HsbBH/'
 f = 'r597.romulus25.3072g1HsbBH.004096'
 
-s = pynbody.load(dpath+f)
+#s = pynbody.load(dpath+f)
 print('loaded')
