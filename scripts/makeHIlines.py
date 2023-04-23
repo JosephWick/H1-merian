dd6f1ad8134f9cf0a6413042a37e7eab8cb4ec0f
Joseph Wick <josephwick@outlook.com>
2023-04-23 15:04:15 -0700

    Update makeHIlines.py

--- scripts/makeHIlines.py
+++ scripts/makeHIlines.py
@@ -103,7 +103,8 @@ def makeHIprofile(hID, withSIDM=False):
     f.close()
     plt.tight_layout()
     if withSIDM:
-        plt.savefig('/home/jw1624/H1-merian/figures/HIProfiles/HI_'+str(hID)+'_2.png')
+        plt.legend(['CDM','SIDM'])
+        axs[0].savefig('/home/jw1624/H1-merian/figures/HIProfiles/HI_'+str(hID)+'_2.png')
     else:
         plt.savefig('/home/jw1624/H1-merian/figures/HIProfiles/HI_'+str(hID)+'.png')
 
