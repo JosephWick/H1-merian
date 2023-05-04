78370cadc4643c35b640d04c5094c6b66f22d79a
Joseph Wick <josephwick@outlook.com>
2023-05-04 11:35:50 -0700

    Update makeSFHFigs.py

--- scripts/makeSFHFigs.py
+++ scripts/makeSFHFigs.py
@@ -8,7 +8,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def sfhFigs(hID, withSIDM=False):
-    fig,axs = plt.subplots(1,2)
+    fig,axs = plt.subplots(1,2, figsize=(12,5))
 
     cdmPath, sidmPath, _ = util.getfilepath(hID)
     cdmFile = cdmPath + '/r'+str(hID)+'.romulus25.3072g1HsbBH.004096'
@@ -29,7 +29,11 @@ def sfhFigs(hID, withSIDM=False):
         sfhsidm2 = pynbody.plot.stars.sfh(sSIDM, subplot=axs[1], massform=False)
 
     # prettify
-    axs[0].set_title('')
+    axs[0].set_title('SFH with Original Star mass')
+    axs[1].set_title('SFH with Final (Observed) Star Mass')
+
+    axs[0].set_xlabel('Gyr')
+    axs[0].set_ylabel('SFR')
 
     if withSIDM:
         plt.savefig('/home/jw1624/H1-merian/figures/sfh/r'+str(hID)+'_SFH_2.png')
