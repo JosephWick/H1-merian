f9ada768493bfa730213718b4cac57f16a3b74aa
Joseph Wick <josephwick@outlook.com>
2023-05-04 11:47:19 -0700

    Update makeSFHFigs.py

--- scripts/makeSFHFigs.py
+++ scripts/makeSFHFigs.py
@@ -10,14 +10,17 @@ import matplotlib.pyplot as plt
 def sfhFigs(hID, withSIDM=False):
     fig,axs = plt.subplots(1,2, figsize=(12,5))
 
+    cdmC = 'firebrick'
+    sidmC= 'royalblue'
+
     cdmPath, sidmPath, _ = util.getfilepath(hID)
     cdmFile = cdmPath + '/r'+str(hID)+'.romulus25.3072g1HsbBH.004096'
 
     sCDM = pynbody.load(cdmFile)
     sCDM.physical_units()
 
-    sfhcdm1 = pynbody.plot.stars.sfh(sCDM, subplot=axs[0])
-    sfhcdm2 = pynbody.plot.stars.sfh(sCDM, subplot=axs[1], massform=False)
+    sfhcdm1 = pynbody.plot.stars.sfh(sCDM, subplot=axs[0], color=cdmC)
+    sfhcdm2 = pynbody.plot.stars.sfh(sCDM, subplot=axs[1], massform=False, color=cdmC)
 
     if withSIDM:
         sidmFile = sidmPath + '/r'+str(hID)+'.romulus25cvdXsec.3072g1HsbBH.004096'
@@ -25,8 +28,8 @@ def sfhFigs(hID, withSIDM=False):
         sSIDM = pynbody.load(sidmFile)
         sSIDM.physical_units()
 
-        sfhsidm1 = pynbody.plot.stars.sfh(sSIDM, subplot=axs[0])
-        sfhsidm2 = pynbody.plot.stars.sfh(sSIDM, subplot=axs[1], massform=False)
+        sfhsidm1 = pynbody.plot.stars.sfh(sSIDM, subplot=axs[0], color='sidmC)
+        sfhsidm2 = pynbody.plot.stars.sfh(sSIDM, subplot=axs[1], massform=False, color='sidmC)
 
     # prettify
     axs[0].set_title('SFH with Original Star mass')
@@ -37,6 +40,10 @@ def sfhFigs(hID, withSIDM=False):
 
     axs[1].set_xlabel('Time [Gyr]')
     axs[1].set_ylabel(r'SFR $[M_{\odot} yr^{-1}]$')
+
+    axs[0].set_legend(['CDM', 'SIDM'])
+
+    plt.tight_layout()
 
     if withSIDM:
         plt.savefig('/home/jw1624/H1-merian/figures/sfh/r'+str(hID)+'_SFH_2.png')
