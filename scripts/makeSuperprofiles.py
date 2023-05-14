a5e289b8883903655dee0c4bb28f7a0b21f6b52d
Joseph Wick <josephwick@outlook.com>
2023-05-14 13:22:31 -0700

    Update makeSuperprofiles.py

--- scripts/makeSuperprofiles.py
+++ scripts/makeSuperprofiles.py
@@ -29,7 +29,7 @@ def gaussian(x, A, mu, s):
 def superprofile(hID, withSIDM=False):
     cdmPath, sidmPath, _ = util.getfilepath(hID)
 
-    i=2
+    print(cdmPath)
     h1files = glob.glob(cdmPath[0]+'/*.fits')
     h1files.sort()
     if len(h1files) == 4: h1files = h1files[1:]
