8686aa9e76df007ca74e8653ae05ee9377f4f8c9
Joseph Wick <josephwick@outlook.com>
2023-06-25 14:53:15 -0700

    Update parseSimFiles.py

--- scripts/parseSimFiles.py
+++ scripts/parseSimFiles.py
@@ -71,6 +71,7 @@ def makeGalQtyCSV(gal, startTS=0):
             continue
 
         print(simFile)
+        print(os.path.isdir(simFile))
         # check for addtional folder
         if os.path.isdir(simFile):
             simFile = simFile+'/r'+str(gal)+'.romulus25.3072g1HsbBH.'+tstepnumber
