eeeed39b4cc078cf270c0c6e572b7ae03b5d1f91
Joseph Wick <josephwick@outlook.com>
2023-07-02 18:07:34 -0700

    Update parseSimFiles.py

--- scripts/parseSimFiles.py
+++ scripts/parseSimFiles.py
@@ -28,6 +28,36 @@ def halfMassRadius(sim, startR, incBy, acc):
 
     return r
 
+def halfMassRadius_bisect(sim, outerR, acc):
+    innerLim = 0.0
+    outerLim = outerR
+    guessR = 0.0
+
+    pRadii = np.array(sim.s['r'])
+    pMass = np.array(sim.s['mass'])
+    mTot = sum(pMass)
+
+    r = outerR
+    hm = mTot
+    while(hm < (0.50-acc)*mTot or hm > (0.5+acc)*mTot):
+        if hm > 0.5*mTot:
+            outerLim = r
+            r = innerLim + (outerLim-innerLim)/2
+        if hm < 0.5*mTot:
+            innerLim = r
+            r = innerLim + (outerLim-innerLim)/2
+        hm = sum(pMass[pRadii < r])
+
+    return r
+
+# Yifei's code for mass weighted 3D velocity distribution
+def compute_vdisp(velocities, masses):
+    center_of_mass_velocity = np.sum(velocities * masses[:, None], axis=0) / np.sum(masses)
+    velocity_differences = velocities - center_of_mass_velocity
+    velocity_differences_squared = np.sum(velocity_differences**2, axis=1)
+    mass_weighted_dispersion = np.sqrt(np.sum(velocity_differences_squared * masses) / np.sum(masses))
+    return mass_weighted_dispersion
+
 #
 def makeGalQtyCSV(gal, startTS=0):
     baseDir = '/data/REPOSITORY/e11Gals/romulus_dwarf_zooms'
@@ -44,6 +74,7 @@ def makeGalQtyCSV(gal, startTS=0):
         fout.write('M_star,R_halflight_s,R_halflight_c,R_halfmass,')
         fout.write('sigma_star,sigma_youngstar,')
         fout.write('log_sigma_pred_10,log_sigma_pred_100,')
+        fout.write('sigma_star_m,sigma_youngstar_m,')
         fout.write('SFR_10,SFR_100,sSFR_10,sSFR_100\n')
         fout.close()
 
@@ -127,11 +158,17 @@ def makeGalQtyCSV(gal, startTS=0):
 
         # velocity dispersion
         #sCDM.properties['boxsize'] = 3.0e4
-        sigma_star= np.median(sCDM.s['v_disp'])
+        sigma_star = np.median(sCDM.s['v_disp'])
         sigma_youngstar = np.median(sCDM.s['v_disp'][sCDM.s['age'].in_units('Myr') < 10])
         # line of sight sigma pred from Hirtenstein et al 2019 eqn (1)
         log_sigma_pred_10 = 0.1006*sSFR_10 + 0.3892*np.log10(mStar) + 0.0126*np.log10(mStar)*sSFR_10
         log_sigma_pred_100= 0.1006*sSFR_100+ 0.3892*np.log10(mStar) + 0.0126*np.log10(mStar)*sSFR_100
+        # manual v disp
+        starVelocity = sCDM.s['velocity']
+        starMass = sCDM.s['mass']
+        agemask = [sCDM.s['age'].in_units('Myr') < 10]
+        sigma_star_m = compute_vdisp(starVelocity, starMass)
+        sigma_youngstar_m =compute_vdisp(starVelocity[agemask], starMass[agemask])
 
         # write to file
         fout.write(str(gal)+','+str(tstepnumber)+','+str(uage)+','+str(stepZ)+',')
@@ -139,6 +176,7 @@ def makeGalQtyCSV(gal, startTS=0):
         fout.write(str(mStar)+','+str(rHL)+','+str(rHL_c)+','+str(rHM)+',')
         fout.write(str(sigma_star)+','+str(sigma_youngstar)+',')
         fout.write(str(log_sigma_pred_10)+','+str(log_sigma_pred_100)+',')
+        fout.write(str(sigma_star_m)+','+str(sigma_youngstar_m)+',')
         fout.write(str(SFR_10)+','+str(SFR_100)+','+str(sSFR_10)+','+str(sSFR_100)+'\n')
 
         fout.close()
