3fa280b8708e4ff6d80b6b72b4b2b46404b59e7a
Joseph Wick <josephwick@outlook.com>
2023-08-05 21:00:21 -0700

    Update utiltest.py

--- util/utiltest.py
+++ util/utiltest.py
@@ -1,3 +1,12 @@
-from util import util
+from util_os import util_os
 
-util.getGalaxies()
+immport pynbody
+
+
+
+##
+
+currentGals = util_os.getGalaxies()
+for gal in currentGals:
+    z0 = util_os.getfilepath(gal,0)
+    sCDM = pynbody.load(zo)
