6de6ddd2deea3a6a8c6fc0a6f74735ee575a77d6
Joseph Wick <josephwick@outlook.com>
2023-08-05 21:58:50 -0700

    Update getAxisRatios.py

--- scripts/getAxisRatios.py
+++ scripts/getAxisRatios.py
@@ -8,26 +8,20 @@ import glob
 
 import sys
 sys.path.insert(0, '../util/')
-from util import util
+from util_os import util_os
+from util_galaxies import util_galaxies
 
 ## command line arguments
 # usage: python3 getAxisRatios galIdx tsIdx radius
 if len(sys.argv) != 4:
-    print('Usage: python3 getAxisRatios [galaxy idx] [ts idx] [galaxy radius]')
+    print('Usage: python3 getAxisRatios [galaxy] [ts idx] [galaxy radius]')
     sys.exit()
 
-gal = util.getGalaxies()[0][int(sys.argv[1])]
+gal = int(sys.argv[1])
 tsidx = int(sys.argv[2])
 R = float(sys.argv[3])
 
-baseDir = '/data/REPOSITORY/e11Gals/romulus_dwarf_zooms'
-galDir = baseDir+ '/r' + str(gal)+'.romulus25.3072g1HsbBH'
-
-timesteps = glob.glob(galDir+'/r*.romulus25.3072g1HsbBH.0*')
-timesteps.sort(reverse=True)
-
-tsnum = timesteps[tsidx][-4:]
-tsfile = glob.glob(timesteps[tsidx]+'/r*.00'+tsnum)[0]
+tsfile = util_os.getfilepath_cdm(gal, tsidx)
 
 sCDM = pynbody.load(tsfile)
 sCDM.physical_units()
@@ -36,11 +30,9 @@ hCDM = -1
 try:
     hCDM = sCDM.halos(write_fpos=False)[1]
 except:
-    # center manually if missing halo; taken from pynbody source code
-    #print('HNF for halo ' + str(gal) + ', timestep '+str(tstepnumber))
-    i = sCDM['phi'].argmin()
-    cen_pot = sCDM['pos'][i].copy()
-    sCDM['pos'] -= cen_pot
+    # center manually if missing halo
+    cen = util_galaxies.compute_CoM(sCDM.s['pos'], sCDM.s['mass'])
+    sCDM['pos'] -= cen
 else:
     hCDM = sCDM.halos(write_fpos=False)[1]
     cen_pot = pynbody.analysis.halo.center(hCDM, mode='pot', retcen=True)
