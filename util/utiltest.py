6fc6b6c93554adf79c42b15a66f147e6111a5aac
Joseph Wick <josephwick@outlook.com>
2023-08-05 21:07:57 -0700

    Update utiltest.py

--- util/utiltest.py
+++ util/utiltest.py
@@ -6,8 +6,8 @@ import pynbody
 
 ##
 
-currentGals = util_os.getGalaxies()
-print(currentGals)
+cdmGals = util_os.getGalaxies()[0]
+print(cdmGals)
 for gal in currentGals:
     z0 = util_os.getfilepath(gal,0)
     sCDM = pynbody.load(z0)
