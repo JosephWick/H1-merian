507f7e500d7520635efabf31e7fb0e2b7f96cabd
Joseph Wick <josephwick@outlook.com>
2023-08-05 21:29:00 -0700

    Update utiltest.py

--- util/utiltest.py
+++ util/utiltest.py
@@ -8,8 +8,16 @@ import pynbody
 
 # test retrieval of z=0
 cdmGals = util_os.getGalaxies()[0]
-print('Testing z=0 retrieval...')
+print('Testing z=0 retrieval...', end='')
 for gal in cdmGals:
     z0 = util_os.getfilepath_cdm(gal, 0)
     sCDM = pynbody.load(z0)
-print('success')
+print('Success\n')
+
+# test retrieval of all CDM timesteps
+for gal in cdmGals:
+    print('Testing all retrieval for r'+str(gal)+'...', end='')
+    nts = util_os.getNumTimesteps(gal)
+    for ts in range(nts):
+        f = util_os.getfilepath_cdm(gal, ts)
+    print('Success')
