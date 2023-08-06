c14ce2395440642927928a3ba50139f9146a2df2
Joseph Wick <josephwick@outlook.com>
2023-08-05 21:16:06 -0700

    Update utiltest.py

--- util/utiltest.py
+++ util/utiltest.py
@@ -9,6 +9,6 @@ import pynbody
 cdmGals = util_os.getGalaxies()[0]
 for gal in cdmGals:
     print('Retrieving z=0 r'+str(gal)+'...', end='')
-    z0 = util_os.getfilepath(gal, 0)
+    z0 = util_os.getfilepath_cdm(gal, 0)
     sCDM = pynbody.load(z0)
     print('done')
