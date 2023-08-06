730701dbcbc3faebe2f65e5fffc5c563a98c6785
Joseph Wick <josephwick@outlook.com>
2023-08-06 14:41:17 -0700

    Update makeEinastoFit.py

--- scripts/makeEinastoFit.py
+++ scripts/makeEinastoFit.py
@@ -39,7 +39,8 @@ def getDMProfile(sCDM):
     pmax = '50 kpc'
 
     # rotation curve
-    pdCDM = pynbody.analysis.profile.Profile(hCDM.d, rmin=pmin, rmax=pmax, type='lin', nbins=500)
+    pdCDM = pynbody.analysis.profile.Profile(hCDM.d, rmin=pmin, rmax=pmax,
+                type='lin', nbins=500)
 
     rbins = pdCDM['rbins']
     dmdensity = pdCDM['density']
