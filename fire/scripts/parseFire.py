6c9e4768fba3c110600d2e5d7ee7c486a80038fc
Joseph Wick <josephwick@outlook.com>
2023-08-10 20:51:02 -0700

    Update parseFire.py

--- fire/scripts/parseFire.py
+++ fire/scripts/parseFire.py
@@ -11,56 +11,12 @@ import numpy as np
 import glob
 import sys
 sys.path.insert(0, '/home/jmwick/H1-merian/util/')
-from util import util
+from util_os import util_os
+from util_galaxies import util_galaxies
 
 ##
 # METHODS
 ##
-
-#
-# positions are 3D; must already be centered
-def compute_Rhalfmass(positions, masses, startR, incBy, acc):
-    radii = np.linalg.norm(correctedpos, ord=2, axis=1)
-    mTot = np.sum(masses)
-
-    # start at startR and increment
-    r = startR - incBy
-    hm = mTot
-    while(hm < (0.50-acc)*mTot or hm > (0.5+acc)*mTot):
-        r+=incBy
-        hm = np.sum(masses[radii < r])
-
-        # check bounds
-        if (r>max(radii)):
-            return -1
-
-    return r
-
-def compute_Rhalfmass_bisect(positions, masses, outerR, acc, maxiter=100000):
-    innerLim = 0.0
-    outerLim = outerR
-
-    pRadii = np.linalg.norm(positions, ord=2, axis=1)
-    mTot = sum(masses)
-
-    r = outerR/2
-    hm = sum(masses[pRadii < r])
-    n=0
-    while(hm < (0.50-acc)*mTot or hm > (0.5+acc)*mTot):
-        if hm > 0.5*mTot: # too big, decrease r
-            outerLim = r
-            r = innerLim + (outerLim-innerLim)/2
-        elif hm < 0.5*mTot: # too small, increase r
-            innerLim = r
-            r = innerLim + (outerLim-innerLim)/2
-        hm = sum(masses[pRadii < r])
-
-        n += 1
-        if n>maxiter:
-            return -1
-            break
-
-    return r
 
 #
 def makeFireCSV(gal):
@@ -93,10 +49,11 @@ def makeFireCSV(gal):
   fout = open(outfile, 'w')
   fout.write('galaxyID,tstep,t,tlookback,z,')
   fout.write('M_star,R_halfmass,')
-  fout.write('sigma_allstars,sigma_allstars_wtd,')
-  fout.write('sigma_youngstar,sigma_youngstar_wtd,')
-  fout.write('sigma_allgas,sigma_allgas_wtd,')
-  fout.write('sigma_coldgas,sigma_coldgas_wtd,')
+  fout.write('sigma_allstars_global,sigma_allstars_los,')
+  fout.write('sigma_youngstar_global,sigma_youngstar_los,')
+  fout.write('sigma_allgas_global,sigma_allgas_los,')
+  fout.write('sigma_coldgas_global,sigma_coldgas_los,')
+  fout.write('alpha,')
   fout.write('SFR_10,SFR_100,sSFR_10,sSFR_100\n')
   fout.close()
 
@@ -105,49 +62,49 @@ def makeFireCSV(gal):
     particles = gizmo.io.Read.read_snapshots('all', 'redshift', z, gal)
 
     # quantities used for deriving other quantities
-    starmasses = particles['star'].prop('mass')
-    starages = particles['star'].prop('age')
-    starpos = particles['star'].prop('position')
-    starvel = particles['star'].prop('host.velocity')
+    mass_allstars = particles['star'].prop('mass')
+    age_allstars = particles['star'].prop('age')
+    pos_allstars = particles['star'].prop('position')
+    vel_allstars = particles['star'].prop('host.velocity')
 
-    agemask = starages<0.01
+    agemask = age_allstars<0.01
 
     # quantities for csv
-    Mstar = np.sum(starmasses)
+    Mstar = np.sum(mass_allstars)
 
     # center of mass
-    posDM = particles['dark'].prop('position')
-    massDM= particles['dark'].prop('mass')
-    com = np.sum(posDM * massDM[:, None], axis=0) / np.sum(massDM)
+    pos_DM = particles['dark'].prop('position')
+    mass_DM= particles['dark'].prop('mass')
+    com = np.sum(pos_DM * mass_DM[:, None], axis=0) / np.sum(mass_DM)
 
-    posDM -= com
-    starpos -= com
-
-    rHM = compute_Rhalfmass_bisect(starpos-com,starmasses, 20000, 0.01)
+    rHM = util_galaxies.compute_massRadius(pos_allstars-com,mass_allstars, 10000, 0.01)
 
     # velocity dispersions
-    vel_youngstars = starvel[agemask]
-    mass_youngstars = starmasses[agemask]
+    pos_youngstars = pos_allstars[agemask]
+    vel_youngstars = vel_allstars[agemask]
+    mass_youngstars = mass_allstars[agemask]
 
-    sigma_star = util.compute_vdisp_std(starvel, starmasses, starvel)
-    sigma_star_wtd = util.compute_vdisp_wtd(starvel, starmasses, starvel,
-                        starmasses)
+    sigma_star_global = util_galaxies.compute_vdisp_global(vel_allstars, mass_allstars,
+                            vel_allstars, mass_allstars)
+    sigma_star_los = util.compute_vdisp_los(vel_allstars, mass_allstars,
+                            pos_allstars-com, vel_allstars, rHM, mass_allstars)
 
-    sigma_youngstar = util.compute_vdisp_std(starvel, starmasses, vel_youngstars)
-    sigma_youngstar_wtd = util.compute_vdisp_wtd(starvel, starmasses,
+    sigma_youngstar_global = util.compute_vdisp_global(vel_allstars, mass_allstars,
                             vel_youngstars, mass_youngstars)
+    sigma_youngstar_los = util.compute_vdisp_los(vel_allstars, mass_allstars,
+                            pos_youngstars-com, vel_youngstars, rHM, mass_youngstars)
 
     pos_allgas = particles['gas'].prop('position')
-    pos_allgas -= com
     vel_allgas = particles['gas'].prop('host.velocity')
     mass_allgas = particles['gas'].prop('mass')
 
-    sigma_allgas = util.compute_vdisp_std(vel_allgas, mass_allgas, vel_allgas)
-    sigma_allgas = util.compute_vdisp_wtd(vel_allgas, mass_allgas, vel_allgas,
-                    mass_allgas)
+    sigma_allgas_global = util.compute_vdisp_global(vel_allgas, mass_allgas,
+                            vel_allgas, mass_allgas)
+    sigma_allgas_los = util.compute_vdisp_los(vel_allgas, mass_allgas,
+                            pos_allgas-com, vel_allgas, rHM, mass_allgas)
 
-    sfr10 = np.sum(starmasses[starages<0.01])
-    sfr100= np.sum(starmasses[starages<0.10])
+    sfr10 = np.sum(mass_allstars[age_allstars<0.01])
+    sfr100= np.sum(mass_allstars[age_allstars<0.10])
 
     ssfr10 = 0
     if sfr10>0: ssfr10 = np.log10(sfr10/(Mstar*1e7))
@@ -155,16 +112,43 @@ def makeFireCSV(gal):
     ssfr100 = 0
     if sfr100>0: ssfr100 = np.log10(sfr100/(Mstar*1e8))
 
+    # can we fit alpha
+
+    # first center DM positions
+    pos_DM -= com
+
+    # we want to fit DM density within inner 2 kpc
+    # get radii
+    radii_DM = np.linalg.norm(pos_DM, axis=1)
+    # TODO
+
+    # QA scatter the dm, stars
+    fig, axs = plt.subplots(1,3, figsize=(15,5), facecolor='w')
+
+    axs[0].scatter(pos_DM[:,0], pos_DM[:,1], c='grey', alpha=0.01, s=1)
+    axs[0].scatter(pos_allstars[:,0], pos_allstars[:,1], c='magenta', s=1)
+
+    axs[1].scatter(pos_DM[:,0], pos_DM[:,2], c='grey', alpha=0.01, s=1)
+    axs[1].scatter(pos_allstars[:,2], pos_allstars[:,1], c='magenta', s=1)
+
+    axs[2].scatter(pos_DM[:,1], pos_DM[:,2], c='grey', alpha=0.01, s=1)
+    axs[2].scatter(pos_allstars[:,1], pos_allstars[:,2], c='magenta', s=1)
+
+    fpath = '/home/jmwick/H1-merian/QA/parse/fire/m11d/'+str(i)+'.png'
+    plt.savefig(fpath)
+    plt.close()
+
     # write to csv
     fout = open(outfile, 'a')
     fout.write(gal[-10:]+','+str(timesteps[i])+','+str(snaptimes[i])+','
                 +str(lookbacks[i])+',')
     fout.write(str(redshifts[i])+',')
     fout.write(str(Mstar)+','+str(rHM)+',')
-    fout.write(str(sigma_star)+','+str(sigma_youngstar)+',')
+    fout.write(str(sigma_star_global)+','+str(sigma_star_los)+',')
+    fout.write(str(sigma_youngstar_global)+','+str(sigma_allstars_los)+',')
+    fout.write(str(sigma_allgas_global)+','+str(sigma_allgas_los)+',')
     fout.write(str(sfr10)+','+str(sfr100)+','+str(ssfr10)+','+str(ssfr100)+'\n')
     fout.close()
-
 
 ##
 # MAIN
